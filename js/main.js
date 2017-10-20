902d671223106d0fc9dec8b5f22871639bb7e645
Speed C <speedchanjun@gmail.com>
2017-10-20 15:08:33 +0800

    Restarted the folders

---
+++ js/main.js
@@ -0,0 +1,19 @@
+console.log("Up and running!");
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You've found a match!")
+	} else{
+		alert("Sorry, try again.")
+	}
+}
