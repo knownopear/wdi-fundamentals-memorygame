3d80f18f070f319d3f2bbb919ce33cfa3dd28150
Speed C <speedchanjun@gmail.com>
2017-10-25 15:01:06 +0800

    finished ex 10

--- js/main.js
+++ js/main.js
@@ -1,25 +1,48 @@
 console.log("Up and running!");
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+{
+rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"
+},
+{
+rank: "queen",
+suit: "diamonds",
+cardImage: "images/queen-of-diamonds.png"
+},
+{
+rank: "king",
+suit: "hearts",
+cardImage: "images/king-of-hearts.png"
+},
+{
+rank: "king",
+suit: "diamonds",
+cardImage: "images/king-of-diamonds.png"
+}
+];
+
 var cardsInPlay = [];
 
 var checkForMatch = function () {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	} else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 };
 
 var flipCard = function (cardId){
-	console.log("User has flipped " + cards[cardId] + "!")
+	console.log("User has flipped " + cards[cardId].rank + "!")
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
 	cardsInPlay.push(cards[cardId]);
-	checkForMatch;
+	checkForMatch();
 };
 
 flipCard(0);
 flipCard(2);
-
 
 
 
