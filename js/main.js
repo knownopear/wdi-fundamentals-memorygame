5a17fee993434d8f156fc53438ca3eb7d66a1121
Speed C <speedchanjun@gmail.com>
2017-11-14 22:36:22 +0800

    finished for now

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-console.log("Up and running!");
-
 var cards = [
 {
 rank: "queen",
@@ -33,16 +31,28 @@ var checkForMatch = function () {
 	}
 };
 
-var flipCard = function (cardId){
-	console.log("User has flipped " + cards[cardId].rank + "!")
-	console.log(cards[cardId].suit);
-	console.log(cards[cardId].cardImage);
+var flipCard = function () {
+	var cardId = this.getAttribute("data-id");
+	this.setAttribute("src", cards[cardId].cardImage);
+	console.log("User flipped " + cards[cardId].rank + "!");
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	if(cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 };
 
-flipCard(0);
-flipCard(2);
+var createBoard = function () {
+    for (var i = 0; i < cards.length; i++) {
+    var cardElement = document.createElement("img");
+    cardElement.setAttribute("src", "images/back.png");
+    cardElement.setAttribute("data-id", i);
+    cardElement.addEventListener("click", flipCard);    
+    document.getElementById("game-board").appendChild(cardElement);
+    }
+};
 
 
 
+createBoard();
